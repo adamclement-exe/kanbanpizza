c527431bc8e83102a2d4c672c09497e09065f0f6
adamclement-exe <57185830+adamclement-exe@users.noreply.github.com>
2025-03-07 16:44:18 +0000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ import eventlet
 eventlet.monkey_patch(os=False, select=True, socket=True, time=True)
 
 # Import the app after monkey-patching
-from app import app
+from main import app
 
 if __name__ == "__main__":
     # For local testing (optional)
