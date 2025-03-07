67334709f1129b5829510bbdd6182603f5f1ae78
adamclement-exe <57185830+adamclement-exe@users.noreply.github.com>
2025-03-07 16:43:07 +0000

    Create wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+# wsgi.py
+import eventlet
+eventlet.monkey_patch(os=False, select=True, socket=True, time=True)
+
+# Import the app after monkey-patching
+from app import app
+
+if __name__ == "__main__":
+    # For local testing (optional)
+    app.run()
