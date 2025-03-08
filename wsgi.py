90267944a3121eade84fe332288b83b8bda83d92
adamclement-exe <57185830+adamclement-exe@users.noreply.github.com>
2025-03-08 21:36:03 +0000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
+# run.py
 import eventlet
 eventlet.monkey_patch()
 
-from main import app
+from main import app, socketio
 
 if __name__ == "__main__":
-    app.run()
+    socketio.run(app, host="0.0.0.0", port=10000)  # Match Render.com's expected port
