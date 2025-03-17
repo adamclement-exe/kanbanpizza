d4548a01b785db63b4fd6d3e52ca9cba75430162
adamclement-exe <57185830+adamclement-exe@users.noreply.github.com>
2025-03-17 14:30:25 +0000

    import send file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import eventlet
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from flask_socketio import SocketIO, emit, join_room
 import time
 import uuid
